886c24fb09a93d90c87722ea11de1ea006e5abce
18luisc <lucero.luis@correounivalle.edu.co>
2023-04-19 09:38:55 -0500

    Actualización ventana Instrucciones

--- MiniProyecto_1/src/vista/VentanaInstrucciones.java
+++ MiniProyecto_1/src/vista/VentanaInstrucciones.java
@@ -7,6 +7,7 @@ package vista;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.GridLayout;
 import java.awt.Image;
 import java.awt.Toolkit;
 import javax.swing.BorderFactory;
@@ -33,6 +34,14 @@ public class VentanaInstrucciones extends JFrame {
     private JLabel instrucciones;
     private JButton btnAtras;
     private String texto;
+    private JLabel jlPalabra;
+    private GridLayout PanelBotones;
+    private JPanel jpBotones;
+    private JButton btnA;
+    private JButton btnE;
+    private JButton btnI;
+    private JButton btnO;
+    private JButton btnU;
     
     public VentanaInstrucciones() {
         initComponents();
@@ -50,6 +59,7 @@ public class VentanaInstrucciones extends JFrame {
         Toolkit miPantalla = Toolkit.getDefaultToolkit();
          
         jpContenido = new JPanel();
+        jpBotones = new JPanel();
         
         String texto = "Fuga de Vocales, es un juego muy divertido\n"
                 +  "y sencillo, para empezar debes hacer lo siguiente:\n" +
@@ -58,7 +68,7 @@ public class VentanaInstrucciones extends JFrame {
                    "\n3. Ver la palabra en pantalla y escoger la vocal que le hace falta. " +
                    "\n4. Eso es todo amigos";
         
-        jlTitulo = new JLabel("FUGA DE LETRAS",SwingConstants.CENTER);
+        jlTitulo = new JLabel("INSTRUCCIONES",SwingConstants.CENTER);
         jlTitulo.setFont(new Font("Showcard Gothic", Font.PLAIN, 36));
         instrucciones = new JLabel(texto);
         instrucciones.setFont(new java.awt.Font("Arial", 0, 25));
@@ -76,15 +86,71 @@ public class VentanaInstrucciones extends JFrame {
         jlTitulo.setBounds(0,20, 520,50);
         jlTitulo.setForeground(Color.BLACK); 
         
-        instrucciones.setBounds(0,100, 510,500);
+        instrucciones.setBounds(10,100, 510,500);
         instrucciones.setForeground(new Color(0,0,0));
         
         jpContenido.add(jlTitulo);
         jpContenido.add(instrucciones);
         
-        btnAtras = new JButton("ATRAS");
+        jlPalabra = new JLabel("GANAST_",SwingConstants.CENTER);
+        jlPalabra.setFont(new java.awt.Font("Arial", 0, 75));
+        jlPalabra.setBounds(0,200,520,75);
+        
+        jpContenido.add(jlPalabra);
+        
+        PanelBotones = new GridLayout(1,5);
+        jpBotones.setLayout(PanelBotones);
+        jpContenido.add(jpBotones);
+        jpBotones.setBackground(new java.awt.Color(178, 221, 185));
+        jpBotones.setSize(450,100);        
+        jpBotones.setBounds(30,300, 450, 100);
+        
+        PanelBotones.setHgap(10);
+        
+        btnA = new JButton("A");
+        btnA.setFont(new java.awt.Font("Britannic Bold", 0, 75));
+//        btnA.setBounds(50,150, 410,50);
+        btnA.setBackground(new java.awt.Color(245, 218, 166));
+        btnA.setForeground(new java.awt.Color(0, 0, 0));
+        btnA.setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));
+        
+        btnE = new JButton("E");
+        btnE.setFont(new java.awt.Font("Britannic Bold", 0, 75));
+//        btnE.setBounds(50,290, 410,50);
+        btnE.setBackground(new java.awt.Color(124, 168, 233));
+        btnE.setForeground(new java.awt.Color(0, 0, 0));
+        btnE.setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));        
+        
+        btnI = new JButton("I");
+        btnI.setFont(new java.awt.Font("Britannic Bold", 0, 75));
+//        btnI.setBounds(50,220, 410,50);
+        btnI.setBackground(new java.awt.Color(245, 218, 166));
+        btnI.setForeground(new java.awt.Color(0, 0, 0));
+        btnI.setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));
+        
+        btnO = new JButton("O");
+        btnO.setFont(new java.awt.Font("Britannic Bold", 0, 75));
+//        btnO.setBounds(50,290, 410,50);
+        btnO.setBackground(new java.awt.Color(245, 218, 166));
+        btnO.setForeground(new java.awt.Color(0, 0, 0));
+        btnO.setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));
+        
+        btnU = new JButton("U");
+        btnU.setFont(new java.awt.Font("Britannic Bold", 0, 75));
+//        btnU.setBounds(50,290, 410,50);
+        btnU.setBackground(new java.awt.Color(245, 218, 166));
+        btnU.setForeground(new java.awt.Color(0, 0, 0));
+        btnU.setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));
+        
+        jpBotones.add(btnA);
+        jpBotones.add(btnE);
+        jpBotones.add(btnI);
+        jpBotones.add(btnO);
+        jpBotones.add(btnU);
+        
+        btnAtras = new JButton("VOLVER");
         btnAtras.setFont(new java.awt.Font("Arial", 0, 25));
-        btnAtras.setBounds(200,400, 100,50);
+        btnAtras.setBounds(180,420, 150,50);
         btnAtras.setBackground(new java.awt.Color(245, 166, 166));
         btnAtras.setForeground(new java.awt.Color(0, 0, 0));
         btnAtras.setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));
