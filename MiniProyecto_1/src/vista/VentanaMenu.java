25a8c11b3612403c89405559765830675a5a1c99
Sebastián Orrego <orrego.sebastian@correounivalle.edu.co>
2023-04-19 22:18:46 -0500

    Requisito Encabezado Añadido

--- MiniProyecto_1/src/vista/VentanaMenu.java
+++ MiniProyecto_1/src/vista/VentanaMenu.java
@@ -2,6 +2,14 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+
+/*
+Autores:  Luis Carlos Lucero A <lucero.luis@correounivalle.edu.co> <202027560>
+          Sebastian Orrego Marin <orrego.sebastian@correounivalle.edu.co> <201941144>
+Grupo: 01
+N° Laboratorio: 01
+Fecha de realización: 20/04/2023
+*/
 package vista;
 
 import java.awt.Color;
